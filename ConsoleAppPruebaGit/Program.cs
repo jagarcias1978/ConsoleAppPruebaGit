b3851b1a5e991342dc7717f469ebced045f97b89
jagarcias1978 <47239976+jagarcias1978@users.noreply.github.com>
2020-09-14 19:39:45 +0200

    Update Program.cs
    Modificado externamente

--- ConsoleAppPruebaGit/Program.cs
+++ ConsoleAppPruebaGit/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleAppPruebaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo!");
+            //modificado desde el navegador
+            Console.WriteLine("he modificado el fichero desde el navegador");
         }
     }
 }
