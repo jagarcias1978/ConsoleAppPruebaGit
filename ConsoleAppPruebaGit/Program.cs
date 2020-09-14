3068f1f5ef775a8ba2d4bde81f998600a7c55869
Jose Alberto <jagarcias@cifpvirgendegracia.com>
2020-09-14 19:35:33 +0200

    Actualización del msg al español

--- ConsoleAppPruebaGit/Program.cs
+++ ConsoleAppPruebaGit/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppPruebaGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo!");
         }
     }
 }
